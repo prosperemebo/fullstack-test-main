e733b28e62352b3867f7e1fb6d3d9558efe8139e
prosperemebo <emeboprosper@gmail.com>
2024-06-20 12:42:52 +0100

    feat(frontend): new entry modal component

---
+++ FrontEnd/src/components/core/entries/NewEntryModal.jsx
@@ -0,0 +1,50 @@
+import { Form, Modal, Radio, Input, Button, Select } from 'antd';
+import { useState } from 'react';
+
+const NewEntryModal = ({ isOpen, handleCancel }) => {
+  const [form] = Form.useForm();
+  const [entryType, setEntryType] = useState('expense');
+
+  const onEntryTypeChange = ({ type }) => {
+    setEntryType(type);
+  };
+
+  return (
+    <Modal open={isOpen} title="Add Entry" onCancel={handleCancel}>
+      <Form
+        layout="vertical"
+        form={form}
+        initialValues={{
+          type: entryType
+        }}
+        onValuesChange={onEntryTypeChange}
+      >
+        <Form.Item label="Entry Type" name="type">
+          <Radio.Group value={entryType}>
+            <Radio.Button value="expense">Expense</Radio.Button>
+            <Radio.Button value="income">Income</Radio.Button>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item label="Amount">
+          <Input placeholder="3000" type="number" />
+        </Form.Item>
+        <Form.Item label="Description">
+          <Input placeholder="New iPhone" />
+        </Form.Item>
+        <Form.Item label="Category">
+          <Select
+            value="null"
+            options={[
+              { value: 'null', label: <span>Select Category</span> },
+              { value: '12345', label: <span>Food</span> },
+              { value: '12345', label: <span>Transport</span> },
+              { value: '12345', label: <span>Food</span> }
+            ]}
+          />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+
+export default NewEntryModal;
--- FrontEnd/src/components/core/layout/EntryHeader.jsx
+++ FrontEnd/src/components/core/layout/EntryHeader.jsx
@@ -1,13 +1,24 @@
+import { useState } from 'react';
 import { Button, Row, Typography } from 'antd';
+import NewEntryModal from '../entries/NewEntryModal';
 
 const { Text } = Typography;
 
-const EntryHeader = () => (
-  <Row className="mb-6 flex items-center justify-between" wrap={false} align="top">
-    <Text className="m-0 truncate text-xl font-normal leading-none">Entries</Text>
-    <Button type="primary" size="large">
-      Add Entry
-    </Button>
-  </Row>
-);
+const EntryHeader = () => {
+  const [isEntryModalOpen, setIsEntryModalOpen] = useState(false);
+
+  const toggleEntryModalHandler = () => setIsEntryModalOpen(state => !state);
+
+  return (
+    <>
+      <Row className="mb-6 flex items-center justify-between" wrap={false} align="top">
+        <Text className="m-0 truncate text-xl font-normal leading-none">Entries</Text>
+        <Button type="primary" size="large" onClick={toggleEntryModalHandler}>
+          Add Entry
+        </Button>
+      </Row>
+      <NewEntryModal isOpen={isEntryModalOpen} handleCancel={toggleEntryModalHandler} />
+    </>
+  );
+};
 export default EntryHeader;
